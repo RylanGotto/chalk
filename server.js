b796fc3e7cd5969f183a635b3bfb14e3d463b0a6
Rylan Gotto <rgotto2@gmail.com>
2015-02-02 17:12:02 -0700

    Updated gcm messages

--- server.js
+++ server.js
@@ -255,7 +255,7 @@ router.route('/users/:user_id')
                         console.log("GCM data not found");
                     }
                     if (user){
-                       var gcmMessage = "You have a friend request from " + req.body.friendusername;
+                       var gcmMessage = "You have a friend request from " + user.username;
                        gcm.sendGcmPushNotification(gcmMessage, [user.token], 2, user.username);
                        console.log("GCM Friend request sent to " + req.body.friendusername);
                     }
