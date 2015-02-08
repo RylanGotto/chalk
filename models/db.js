fe0e85f8306141a81cef5ed26732efc77f4ac275
Rylan Gotto <rgotto2@gmail.com>
2015-02-08 13:55:04 -0700

    Merged with rylan and updated db url

--- models/db.js
+++ models/db.js
@@ -1,6 +1,6 @@
 var mongoose = require('../node_modules/mongoose');
-var dbName = "uppercut";
-mongoose.connect('mongodb://localhost:27017/' + dbName, function () {
+var dbName = "chalk";
+mongoose.connect('mongodb://dbuser:dbuser@ds061620.mongolab.com:61620/chalk', function () {
     console.log('chalkin it up on ' + dbName + "!");
 });
 module.exports = mongoose;
