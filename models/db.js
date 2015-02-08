ddb335eda15b33bcf1db5f8776fa13e3cd30b574
Rylan Gotto <rgotto2@gmail.com>
2015-02-08 16:29:10 -0700

    db change

--- models/db.js
+++ models/db.js
@@ -1,5 +1,5 @@
 var mongoose = require('../node_modules/mongoose');
-var dbName = "uppercut";
+var dbName = "rylan";
 mongoose.connect('mongodb://localhost:27017/' + dbName, function () {
     console.log('chalkin it up on ' + dbName + "!");
 });
