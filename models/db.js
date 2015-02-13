992889ae54ecde07cf636e4e529b6929597fb550
Rylan Gotto <rgotto2@gmail.com>
2015-02-12 20:35:12 -0700

    update to mike

--- models/db.js
+++ models/db.js
@@ -1,5 +1,5 @@
 var mongoose = require('../node_modules/mongoose');
-var dbName = "rygan1";
+var dbName = "chalk";
 mongoose.connect('mongodb://localhost:27017/' + dbName, function () {
     console.log('chalkin it up on ' + dbName + "!");
 });
