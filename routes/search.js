f369bc5ac8fda3667a9f42f03beb3a4fabd552fa
Michael McCulloch <michael.james.mcculloch@gmail.com>
2015-02-22 17:31:20 -0700

    Added permissions for search module

--- routes/search.js
+++ routes/search.js
@@ -5,8 +5,10 @@
 var express = require('express');
 var router = express.Router();
 var DataModel = require('../models/datamodel');
+var permissions = require('../lib/permissions');
+
 router.route('/search')
-    .post(function(req, res, next){
+    .post(function(req, res, next) {
         if (!req.auth) {
             return res.status(401).send();
         }
@@ -17,12 +19,15 @@ router.route('/search')
         var regexLast = new RegExp(searchTerms[1], 'i');
         var users = [];
         var userID = [];
-        if(searchTerms.length == 2) { //assume element zero is first name, and element one is second name and look for matches.
-            //also look for board tag based on the entire search query
 
+        if (searchTerms.length == 2) {
+            search(regexSearch, regexLast, regexFirst)
+        } else {
+            search(regexSearch, regexSearch, regexSearch)
+        }
 
-
-            DataModel.User.find({$or: [{username: regexSearch}, {lastname: regexLast}, {firstname: regexFirst}]}).exec(function (err, matches) {
+        function search(term1, term2, term3) {
+            DataModel.User.find({$or: [{username: term1}, {lastname: term2}, {firstname: term3}]}).exec(function (err, matches) {
 
                 if (matches) {
                     matches.forEach(function (user) {
@@ -31,62 +36,61 @@ router.route('/search')
                         userID.push(user._id);
                     });
                 }
-                DataModel.Board.find({owner: {$not: {$in:userID}}, tag: regexSearch}).exec(function (err, boards) {
+                DataModel.Board.find({
+                    owner: {$not: {$in: userID}},
+                    tag: term1
+                }).populate('owner').exec(function (err, boards) {
                     if (err) {
                         return next(err);
                     }
                     if (!boards) {
                         return res.status(401).json(jmsg.board_ex);
-                    }else{
-                        return res.status(200).send({users: users, boards: boards});
+                    } else {
+                        var data = [];
+
+                        DataModel.User.findOne({_id: {$in: [req.auth.id]}}).exec(function (err, user) {
+                            boards.forEach(function (board) {
+                                var isFriendsOrMutualFriend = (permissions.isFriend(board.owner.friends, req.auth.id) || permissions.hasMutualFriends(board.owner.friends, user.friends));
+                                if (board.privacyLevel == 'Public') {
+                                    console.log(board.tag);
+                                    data.push(board);
+                                } else if ((board.privacyLevel == 'Friends') && (isFriendsOrMutualFriend)) {
+                                    console.log(board.tag);
+                                    data.push(board);
+                                } else if ((board.privacyLevel == 'Private') && (isFriendsOrMutualFriend) && (permissions.isPermitted(board, req.auth.id))) {
+                                    console.log(board.tag);
+                                    data.push(board);
+                                }
+                            });
+                            return res.status(200).send({users: users, boards: data});
+                        });
                     }
+
+                    /*
+
+
+
+                     boards.forEach(function (board){
+                     DataModel.User.findOne({_id: {$in: [board.owner]}}).exec(function(err, owner){
+                     DataModel.User.findOne({_id: {$in: [req.auth.id]}}).exec(function(err, user){
+                     isFriendsOrMutualFriend = (permissions.isFriend(owner.friends, req.auth.id) || permissions.hasMutualFriends(owner.friends, user.friends));
+                     if (board.privacyLevel == 'Public'){
+                     data.push(board);
+                     } else if ((board.privacyLevel == 'Friends') && (isFriendsOrMutualFriend)){
+                     data.push(board);
+                     } else if ((board.privacyLevel == 'Private') && (isFriendsOrMutualFriend) && (permissions.isPermitted(board, req.auth.id))){
+                     data.push(board);
+                     }
+                     });
+                     });
+                     });
+                     return res.status(200).send({users: users, boards: data}); //data is empty here, should have boards
+                     }
+
+                     */
                 });
             });
-        }
-        else{ //search username firstname last name and board
-
-            DataModel.User.find({$or: [{username: regexSearch}, {lastname: regexSearch}, {firstname: regexSearch}]}).exec(function(err, matches){
-
-                if(matches){
-                    matches.forEach(function(user){
-                        userInfo = user.username + " - " + user.firstname + " " + user.lastname;
-                        users.push({userInfo: userInfo, username: user.username});
-                        userID.push(user._id);
-                    });
-                }
-                DataModel.Board.find({owner: {$not: {$in:userID}}, tag: regexSearch}).exec(function (err, boards) {
-                    if (err) {
-                        return next(err);
-                    }
-                    console.log(boards);
-                    if (!boards) {
-                        return res.status(401).json(jmsg.board_ex);
-                    }else{
-                        return res.status(200).send({users: users, boards: boards});
-                    }
-                });
-
-
-
-
-
-
-
-
-            });
-
-
-
         }
 
     });
-
-
 module.exports = router;
-
-
-
-
-
-
-
